043bc286603524bb6c3875be6e87ac4f4ba3ea5a
sachin_dashzer <sachin8287037611@gmail.com>
2023-09-25 16:22:44 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
           <Route path="*" element={<Pagenotfound />} />
         </Routes>
 
-        {/* <Footer /> */}
+        <Footer />
       </Router>
     </>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,40 +9,127 @@ import Mainbox from "../Mainbox/Mainbox";
 import "./header.scss";
 
 const Header = () => {
+
+  const [showNavbar, setShowNavbar] = useState("top")
+  const [lastscrollY , setlastscrollY] = useState(0)
+
   const [menubtn, setmenubtn] = useState(false);
+  const [searchbtn, setsearchbtn] = useState(false);
+  const [query, setquery] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const showbtns = () => {
-    setmenubtn(!menubtn);
-  };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
-  const setNavigation = (x)=>{
-    navigate(`/explore/${x}`)
-    showbtns();
+
+  const setNavbar = ()=>{
+
+    if(window.scrollY > 200){
+
+      if(window.scrollY > lastscrollY && !menubtn){
+        setShowNavbar("hide");
+      }
+      else{
+        setShowNavbar("show");
+      }
+
+    }
+    else{
+      setShowNavbar("top");
+    }
+
+    setlastscrollY(window.scrollY);
   }
 
+
+  useEffect(()=>{
+
+    window.addEventListener("scroll" , setNavbar);
+
+    return() =>{
+      window.removeEventListener("scroll" , setNavbar)
+    }
+
+  }, [lastscrollY])
+
+
+  function setqueryhandeler(event) {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+
+      setTimeout(() => {
+        setsearchbtn(false);
+      }, 1000);
+    }
+  }
+
+  const showbtns = () => {
+    setmenubtn(true);
+    setsearchbtn(false);
+  };
+
+  const showsearchbtns = () => {
+    setmenubtn(false);
+    setsearchbtn(true);
+  };
+
+  const setNavigation = (x) => {
+    navigate(`/explore/${x}`);
+    showbtns();
+  };
+
   return (
-    <div className="header">
+    <div className={`header ${showNavbar}`}>
       <Mainbox>
         <div className="logo" onClick={() => navigate("/")}>
           <img src={Logo} alt="" />
           <h1>Fimax</h1>
         </div>
         <ul className={`menu ${menubtn ? "menuactive" : ""}`}>
-          <li className="menubars" onClick={()=>setNavigation("movies")}>movies</li>
-          <li className="menubars" onClick={()=>setNavigation("tv")}>Tv show</li>
-          <HiOutlineSearch style={{ fontSize: "20px" }} />
+          <li className="menubars" onClick={() => setNavigation("movies")}>
+            movies
+          </li>
+          <li className="menubars" onClick={() => setNavigation("tv")}>
+            Tv show
+          </li>
+          <HiOutlineSearch
+            onClick={showsearchbtns}
+            style={{ fontSize: "20px" }}
+          />
         </ul>
 
         <ul className="mobile-menu">
           <li className="menubars">
-            <HiOutlineSearch />
+            <HiOutlineSearch onClick={showsearchbtns} />
           </li>
           <li className="menubars">
-            <SlMenu onClick={showbtns} />
+            {menubtn ? (
+              <VscChromeClose onClick={() => setmenubtn(false)} />
+            ) : (
+              <SlMenu onClick={showbtns} />
+            )}
           </li>
         </ul>
       </Mainbox>
+
+      {searchbtn && (
+        <div className="searchbar">
+          <Mainbox>
+            <div className="searchbox">
+              <input
+                type="text"
+                placeholder="Search a Movie or Tv show"
+                onChange={(e) => setquery(e.target.value)}
+                onKeyUp={setqueryhandeler}
+              />
+
+              <VscChromeClose onClick={() => setsearchbtn(false)} />
+            </div>
+          </Mainbox>
+        </div>
+      )}
     </div>
   );
 };
